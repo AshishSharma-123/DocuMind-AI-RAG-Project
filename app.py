64073883ccf8775b2486f4412e477fae938518aa
AshishSharma-123 <AshishSharmahr16@gmail.com>
2026-05-24 16:02:01 +0530

    Prepare for deployment

--- app.py
+++ app.py
@@ -178,8 +178,5 @@ def ask_question():
 # ==========================================
 # 5. RUN SERVER
 # ==========================================
-if __name__ == '__main__':
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
-
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
